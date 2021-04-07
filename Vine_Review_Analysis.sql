d6c9dea47180073a8cc65fe3b72b46cad6387870
NataliaVelasquez18 <nataliavelasquez@natalias-mbp.lan>
2021-04-07 18:58:25 -0500

    adding sql file reviewed

--- Vine_Review_Analysis.sql
+++ Vine_Review_Analysis.sql
@@ -20,7 +20,7 @@ WHERE total_votes >= 20;
 -- See my new created table
 SELECT * FROM votes_over_20 LIMIT 5;
 
--- From votes_over_20 create new table wherehelpful_votes divided 
+-- From votes_over_20 create new table where helpful_votes divided 
 -- by total_votes is equal to or greater than 50%.
 
 SELECT *
@@ -40,57 +40,136 @@ WHERE vine = 'Y';
 -- New Table where review was written as not part of the Vine program (unpaid)
 
 SELECT *
-INTO nonpaid_reviews
+INTO unpaid_reviews
 FROM helpful_votes_over50_percent
 WHERE vine = 'N';
 
 
 
--- total number of reviews: 40,565
-SELECT COUNT( DISTINCT review_id)
-FROM helpful_votes_over50_percent;
+-- Create a table for the number of total paid reviews
+CREATE TABLE total_paid_reviews (
+  total_reviews INT
+);
 
--- number of 5-star reviews: 15,711
-SELECT COUNT( DISTINCT review_id)
-FROM helpful_votes_over50_percent
+--- fill up the new table for total paid reviews
+INSERT INTO total_paid_reviews(total_reviews)
+SELECT COUNT(vine)
+FROM paid_reviews;
+
+-- Check my table
+SELECT * FROM total_paid_reviews;
+
+
+-- Create a table for the number of total paid 5 star reviews
+CREATE TABLE paid_five_star_reviews (
+  paid_five_S_reviews INT
+);
+
+-- fill up second table for total_five_star_paid reviews
+INSERT INTO paid_five_star_reviews(paid_five_S_reviews)
+SELECT COUNT(vine)
+FROM paid_reviews
 WHERE star_rating = 5;
 
--- 5 star paid reviews: 48
-SELECT COUNT( DISTINCT review_id) AS Helpful_Paid_5star
-FROM helpful_votes_over50_percent
-WHERE star_rating = 5 
-AND vine = 'Y';
 
+-- check my new table
+SELECT * 
+FROM paid_five_star_reviews;
 
---- 5 star non-paid reviews: 15,663
-SELECT COUNT( DISTINCT review_id) AS Helpful_NonPaid_5star
-FROM helpful_votes_over50_percent
-WHERE star_rating = 5 
-AND vine = 'N';
+-- FIND Tthe percentage of 5 star reviews out of total (paid)
+SELECT TPR.total_reviews, PFSR.paid_five_S_reviews
+INTO percentage_paid
+FROM total_paid_reviews as TPR
+INNER JOIN paid_five_star_reviews as PFSR
+ON 1=1;
 
+SELECT * FROM percentage_paid;
 
 -- percentage of 5-star reviews for paid reviews: 
-SELECT 
-(COUNT(DISTINCT review_id) * 100
-FROM helpful_votes_over50_percent
-WHERE star_rating = 5
-AND vine = 'Y') / 
- 
-(SELECT COUNT( DISTINCT review_id)
-FROM helpful_votes_over50_percent
-WHERE star_rating = 5 );
+SELECT total_reviews, paid_five_S_reviews,
+CONCAT(ROUND (CAST(paid_five_S_reviews AS FLOAT)/
+	CAST(total_reviews AS FLOAT)*100), '%')
+	As percent_paid_five
+INTO percent_paid_five_analysis
+FROM percentage_paid;
 
+-------------------
+
+
+SELECT *
+FROM percent_paid_five_analysis;
+
+
+
+-------------------
+
+
+
+
+
+-- Create a table for the number of total unpaid reviews
+CREATE TABLE total_unpaid_reviews (
+  total_reviews INT
+);
+
+--- fill up the new table for total unpaid reviews
+INSERT INTO total_unpaid_reviews(total_reviews)
+SELECT COUNT(vine)
+FROM unpaid_reviews;
+
+-- Check my table
+SELECT * FROM total_unpaid_reviews;
+
+
+-- Create a table for the number of total unpaid 5 star reviews
+CREATE TABLE unpaid_five_star_reviews (
+  unpaid_five_S_reviews INT
+);
+
+-- fill up second table for total_five_star_unpaid reviews
+INSERT INTO unpaid_five_star_reviews(unpaid_five_S_reviews)
+SELECT COUNT(vine)
+FROM unpaid_reviews
+WHERE star_rating = 5;
+
+
+-- check my new table
+SELECT * 
+FROM unpaid_five_star_reviews;
+
+-- FIND Tthe percentage of 5 star reviews out of total (unpaid)
+SELECT TUPR.total_reviews, UPFSR.unpaid_five_S_reviews
+INTO percentage_unpaid
+FROM total_unpaid_reviews as TUPR
+INNER JOIN unpaid_five_star_reviews as UPFSR
+ON 1=1;
+
+SELECT * FROM percentage_unpaid;
 
 -- percentage of 5-star reviews for unpaid reviews: 
 SELECT 
-(COUNT(DISTINCT review_id) * 100
-FROM helpful_votes_over50_percent
-WHERE star_rating = 5
-AND vine = 'N') / 
- 
-(SELECT COUNT( DISTINCT review_id)
-FROM helpful_votes_over50_percent
-WHERE star_rating = 5 );
+total_reviews, 
+unpaid_five_S_reviews,
+CONCAT(ROUND (CAST(unpaid_five_S_reviews AS FLOAT)/
+			  CAST(total_reviews AS FLOAT)*100), '%') 
+			  As percent_unpaid_five 
+--INTO percent_unpaid_five_analysis
+FROM percentage_unpaid;
+
+
+------------
+
+SELECT *
+FROM percent_unpaid_five_analysis;
+
+-------------
+
+
+
+
+
+
+
 
 
 
